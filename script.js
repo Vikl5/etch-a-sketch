7923b56dcfa73650dc86c5491bc682a2e5744d3a
Vikl5 <Vikl5@protonmail.com>
2022-10-09 13:36:48 +0200

    Added RGB coloring and a reset button
    Changed the color from green to red on hover. The reset button will now
    display a blank grid and the RGB button will now change to a random
    color for each div.

--- script.js
+++ script.js
@@ -1,25 +1,60 @@
 const container = document.querySelector("#container");
+const resetButton = document.querySelector("#resetButton");
+const rgbButton = document.querySelector("#rgbButton");
+const redButton = document.querySelector("#redButton");
 
 function createDivs(rows, columns){
-    // container.style.setProperty("grid-rows", rows);
-    // container.style.setProperty("grid-columns", columns);
     for (let i = 0; i < rows * columns; i++) {
         let divs = document.createElement("div");
-        //divs.innerText = (i + 1);
         divs.setAttribute("class", 
-        "h-8 w-8 border border-gray-600 hover:bg-green-500")
+        "h-8 w-8 border border-gray-600")
+        divs.classList.add("gridSquare");
         container.appendChild(divs);
+        divs.addEventListener("mouseenter", function() {
+            divs.style.background = "red";
+        })
     }
-    // let arr = []
-    // for (let i = 0; i < rows * columns; i++) {
-    //     const element = <div class="h-8 w-8 border border-gray-600" ></div>;
-    //     arr.push(element)
-    // }
-    // container.appendChild(arr);
 }
+
+function resetGrid() {
+    let gridSquare = document.querySelectorAll("div.gridSquare")
+    gridSquare.forEach(element => {
+        element.style.background = "rgb(203 213 225)"
+    });
+}
+
 
 function makeSquares(input){
     let 
 }
 
+
+function rgbColor() {
+    let gridSquare = document.querySelectorAll("div.gridSquare");
+    gridSquare.forEach(element => {
+        element.addEventListener("mouseenter", () => {
+            let r = Math.floor(Math.random() * 256);
+            let g = Math.floor(Math.random() * 256);
+            let b = Math.floor(Math.random() * 256);
+
+            element.style.background = `rgb(${r}, ${g}, ${b})`;
+        })
+    })
+}
+
+function redColor() {
+    let gridSquare = document.querySelectorAll("div.gridSquare");
+    gridSquare.forEach(element => {
+        element.addEventListener("mouseenter", () => {
+            element.style.background = "red";
+
+        })
+    })
+}
+
+
 createDivs(16, 16);
+
+resetButton.addEventListener("click", resetGrid);
+rgbButton.addEventListener("click", rgbColor);
+redButton.addEventListener("click", redColor);
